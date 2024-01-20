e53da01f5285b68cc198ff1de59b39f77172ebc1
ags <andrzej.grzesik@gmail.com>
2024-01-20 20:31:18 +0000

    parametrise tests across locales

--- Testing/WaypointParserUnitTests.cs
+++ Testing/WaypointParserUnitTests.cs
@@ -1,40 +1,56 @@
+using System.Globalization;
 using DTC.Models.v476;
 using Microsoft.VisualBasic.CompilerServices;
+using static DTC.Models.v476.RecordType;
 
 namespace Testing
 {
+    [TestFixture("en-US")]
+    [TestFixture("en-GB")] 
+    [TestFixture("pl-PL")]
+    [TestFixture("de-DE")]
     public class WaypointParserUnitTests
     {
         private WaypointSystemParser _parser;
+        private string locale;
+
+        public WaypointParserUnitTests(string locale)
+        {
+            this.locale = locale;
+        }
 
         [SetUp]
         public void Setup()
         {
             _parser = new WaypointSystemParser();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(locale);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(locale);
         }
 
         [Test]
-        public void shouldDetectTopLine()
+        public void Should_DetectTopLine()
         {
-            Assert.That(RecordType.FlightPlanIndicator, Is.EqualTo(_parser.detect("Flight Plan")));
+            Assert.That(_parser.detect("Flight Plan"), Is.EqualTo(FlightPlanIndicator));
         }
 
         [Test]
-        public void shouldDetectFlightPlanHeader()
+        public void Should_DetectFlightPlanHeader()
         {
-            Assert.That(RecordType.FlightPlanHeader, Is.EqualTo(_parser.detect("#	Type	Name	Fix/Point	Location	Elev	Alt	SPD	ETE/TOT")));
+            Assert.That(_parser.detect("#	Type	Name	Fix/Point	Location	Elev	Alt	SPD	ETE/TOT"),
+                Is.EqualTo(FlightPlanHeader));
         }
 
         [Test]
         public void shouldDetectWaypoint()
         {
-            Assert.That(RecordType.Waypoint, Is.EqualTo(_parser.detect("13\tOBJ\t\t4808A.1\tN 37\u00b028.000' W 116\u00b000.050'")));
+            Assert.That(_parser.detect("13\tOBJ\t\t4808A.1\tN 37\u00b028.000' W 116\u00b000.050'"),
+                Is.EqualTo(Waypoint));
         }
-       
+
         [Test]
         public void DetectLine_ReturnsEmptyRow_ForEmptyWaypoint()
         {
-            Assert.That(_parser.detect("17\t\t\t\t\t\t\t\t\t"), Is.EqualTo(RecordType.EmptyRow)); 
+            Assert.That(_parser.detect("17\t\t\t\t\t\t\t\t\t"), Is.EqualTo(EmptyRow));
         }
 
         [Test]
@@ -42,6 +58,7 @@ namespace Testing
         {
             _parser.parseForF16(Data.viperPlanWithNotes);
         }
+
         [Test]
         public void ParseForFA18_ParsesTestInput()
         {
@@ -56,29 +73,29 @@ namespace Testing
             Assert.That(WaypointBuilder.ParseHeightData("1234"), Is.EqualTo(1234));
             Assert.That(WaypointBuilder.ParseHeightData(""), Is.Null);
         }
-        
+
         [Test]
         public void Parser_Understands_MGRS()
         {
             var maybeTuple = WaypointBuilder.ParseLatLong("11SNB7237453149");
-            
+
             Assert.That(maybeTuple, Is.Not.Null);
             Assert.That(maybeTuple?.Item1._hemisphere, Is.EqualTo(Longitude.Hemisphere.West));
             Assert.That(maybeTuple?.Item2._hemisphere, Is.EqualTo(Latitude.Hemisphere.North));
-            
+
             Assert.That(maybeTuple?.Item1.ddmmmm.ddmm_mmm(), Is.EqualTo("116\u00b010.858’"));
             Assert.That(maybeTuple?.Item2.ddmmmm.ddmm_mmm(), Is.EqualTo("37\u00b031.348’"));
         }
-        
+
         [Test]
         public void Parser_Troublesome_Coordinates()
         {
             var maybeTuple = WaypointBuilder.ParseLatLong("N 36\u00b018,617' W 115\u00b002,367'");
-            
+
             Assert.That(maybeTuple, Is.Not.Null);
             Assert.That(maybeTuple?.Item1._hemisphere, Is.EqualTo(Longitude.Hemisphere.West));
             Assert.That(maybeTuple?.Item2._hemisphere, Is.EqualTo(Latitude.Hemisphere.North));
-            
+
             Assert.That(maybeTuple?.Item1.ddmmmm.ddmm_mmm(), Is.EqualTo("115\u00b002.367’"));
             Assert.That(maybeTuple?.Item2.ddmmmm.ddmm_mmm(), Is.EqualTo("36\u00b018.617’"));
         }
