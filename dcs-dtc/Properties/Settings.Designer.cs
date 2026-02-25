e5240d52582a48ade0fa2ed051ec6c6e1b243beb
Rafał Szul <rafal.szul@gmail.com>
2026-02-25 21:15:36 +0100

    9.0.0 merge and release files. Old DTC must grow yet.

--- dcs-dtc/Properties/Settings.Designer.cs
+++ dcs-dtc/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DTC.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace DTC.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2024.11.15")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2026.02.24")]
         public string v476thBuild {
             get {
                 return ((string)(this["v476thBuild"]));
